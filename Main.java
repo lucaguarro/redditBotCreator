60560823f9659898cb6933f413cbf69dede5c1a3
Luca Guarro <luca.guarro@gmail.com>
2018-03-08 18:10:06 -0800

    Created and hosting helper app to authorize slack login

--- Main.java
+++ Main.java
@@ -3,16 +3,19 @@ package redditBotCreator;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import org.json.JSONObject;
+//import redditBotCreator.slack;
 //import redditBotCreator.reddit;
 
 
 public class Main extends Application {
 	private static reddit r = new reddit();
+	private static Slack s = new Slack();
     public static void main(String[] args) {
         launch(args);
     }
@@ -21,21 +24,34 @@ public class Main extends Application {
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
-        btn.setText("Say 'Hello World!!!!'");
+        btn.setText("Test Reddit Request");
         btn.setOnAction(new EventHandler<ActionEvent>() {
  
-        @Override
-        public void handle(ActionEvent event) {
-                System.out.println("Making reddit request");
-        		JSONObject data;
-        		data = r.redditTest();
-        		data = data.getJSONObject("data");
-                System.out.println(data.toString());
+            @Override
+            public void handle(ActionEvent event) {
+                    System.out.println("Making reddit request");
+                    JSONObject data;
+                    data = r.redditTest();
+                    data = data.getJSONObject("data");
+                    System.out.println(data.toString());
             }
         });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+
+        Button slackBtn = new Button();
+        slackBtn.setText("Login to Slack");
+        slackBtn.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                s.openWebxdg();
+            }
+        });
+
+        BorderPane root = new BorderPane();
+        root.setLeft(btn);
+        root.setRight(slackBtn);
+        /*StackPane root = new StackPane();
+        root.getChildren().add(slackBtn);
+        root.getChildren().add(btn);*/
         primaryStage.setScene(new Scene(root, 300, 250));
         primaryStage.show();
     }
--- Slack.java
+++ Slack.java
@@ -1,5 +1,45 @@
-package application;
+package redditBotCreator;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 public class Slack {
 
+    void openWebxdg(){
+        String url = "http://www.redditbotcreator.byethost6.com/apps/myapp/install.php";
+        Runtime runtime = Runtime.getRuntime();
+        try {
+            runtime.exec("xdg-open " + url);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+    void openWeb2() {
+        String url = "http://www.google.com";
+
+        if(Desktop.isDesktopSupported()){
+            Desktop desktop = Desktop.getDesktop();
+            try {
+                desktop.browse(new URI(url));
+            } catch (IOException | URISyntaxException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }else{
+            Runtime runtime = Runtime.getRuntime();
+            try {
+                runtime.exec("xdg-open " + url);
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+    void openWeb() {
+        if (Desktop.isDesktopSupported()) {
+            //Desktop.getDesktop().browse(new URI("http://www.example.com"));
+        }
+    }
 }
