a11c6ca692054a5a55f26fe70bc2f4400fa00f17
Luca Guarro <luca.guarro@gmail.com>
2018-04-25 23:33:59 -0700

    new bot button is actually adding something to list now

--- Controller.java
+++ Controller.java
@@ -1,10 +1,20 @@
 package redditBotCreator;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Paint;
+import javafx.stage.Stage;
+import javafx.util.Callback;
 import org.json.JSONObject;
 import javafx.fxml.Initializable;
 
@@ -31,11 +41,26 @@ public class Controller implements Initializable {
     @FXML
     private TableColumn<Bot, String> nameColumn;
     @FXML
-    private TableColumn<Bot, CheckBox> statusColumn;
+    private TableColumn<Bot, Boolean> statusColumn;
 
     public void initialize(URL location, ResourceBundle resources){
         nameColumn.setCellValueFactory(new PropertyValueFactory<Bot, String>("name"));
-        statusColumn.setCellValueFactory(new PropertyValueFactory<Bot, CheckBox>("on"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<Bot, Boolean>("on"));
+        statusColumn.setSortable(false);
+
+        statusColumn.setCellValueFactory(new Callback<TableColumn.CellDataFeatures<Bot, Boolean>, ObservableValue<Boolean>>(){
+            @Override public ObservableValue<Boolean> call(TableColumn.CellDataFeatures<Bot, Boolean> features) {
+                return new SimpleBooleanProperty(features.getValue() != null);
+            }
+        });
+
+        // create a cell value factory with an add button for each row in the table.
+        statusColumn.setCellFactory(new Callback<TableColumn<Bot, Boolean>, TableCell<Bot, Boolean>>() {
+            @Override public TableCell<Bot, Boolean> call(TableColumn<Bot, Boolean> personBooleanTableColumn) {
+                return new AddBotCell(/*stage, botTable*/);
+            }
+        });
+
         botTable.setItems(bots);
     }
     /*public ObservableList<Bot> getBots(){
@@ -55,9 +80,6 @@ public class Controller implements Initializable {
     public void onBotCreate(){
         String name = newBotName.getText();
         bots.add(new Bot(name));
-        //Bot bot = new Bot(name);
-        //bots.add(bot);
-        //addBotToTable(bot);
     }
 
     public void testRedditRequest(){
@@ -68,4 +90,34 @@ public class Controller implements Initializable {
         System.out.println(data.toString());
     }
 
+    /** A table cell containing a button for adding a new person. */
+    private class AddBotCell extends TableCell<Bot, Boolean> {
+
+        // a button for adding a new person.
+        final CheckBox checkbox = new CheckBox();
+        // pads and centers the add button in the cell.
+        final StackPane paddedCheckBox = new StackPane();
+        // records the y pos of the last button press so that the add person dialog can be shown next to the cell.
+        final DoubleProperty buttonY = new SimpleDoubleProperty();
+
+        AddBotCell(/*final Stage stage, final TableView table*/) {
+            paddedCheckBox.setPadding(new Insets(3));
+            paddedCheckBox.getChildren().add(checkbox);
+            checkbox.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+                @Override
+                public void handle(MouseEvent event) {
+                }
+            });
+        }
+
+        /** places an add button in the row only if the row is not empty. */
+        @Override protected void updateItem(Boolean item, boolean empty) {
+            super.updateItem(item, empty);
+            if (!empty) {
+                setContentDisplay(ContentDisplay.GRAPHIC_ONLY);
+                setGraphic(checkbox);
+            }
+        }
+    }
 }
