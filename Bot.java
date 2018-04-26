6bd430ab7f4e4c32ed26d9dc0803f089d2b4eadf
Luca Guarro <luca.guarro@gmail.com>
2018-04-25 17:58:58 -0700

    can now add bots to table view dynamically with new bot button

--- Bot.java
+++ Bot.java
@@ -1,14 +1,19 @@
 package redditBotCreator;
 
+import javafx.beans.property.SimpleStringProperty;
+
+
 import java.util.ArrayList;
 
 public class Bot {
-    String name;
+    private SimpleStringProperty name;
     private int frequency;
     private ArrayList<String> subreddits;
     private ArrayList<String> words;
+    private boolean isOn;
     public Bot(String name){
-        this.name = name;
+        this.name = new SimpleStringProperty(name);
+        this.turnOff();
     }
     public int getFrequency(){ return frequency; }
     public void setFrequency(int frequency){ this.frequency = frequency; }
@@ -16,6 +21,9 @@ public class Bot {
     public void setSubreddits(ArrayList<String> subreddits){ this.subreddits = subreddits; }
     public ArrayList<String> getWords(){ return words; }
     public void setWords(ArrayList<String> words){ this.words = words; }
-    public String getName(){ return name; }
-    public void setName(String name){ this.name = name; }
+    public String getName(){ return name.get(); }
+    public void setName(String name){ this.name.setValue(name); }
+    public Boolean isOn(){ return this.isOn; }
+    public void turnOff(){ this.isOn = false; }
+    public void turnOn(){ this.isOn = true; }
 }
--- Controller.java
+++ Controller.java
@@ -1,27 +1,48 @@
 package redditBotCreator;
 import de.jensd.fx.glyphs.fontawesome.FontAwesomeIconView;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.*;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.paint.Paint;
 import org.json.JSONObject;
 import javafx.fxml.Initializable;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;//ab2525
 
 public class Controller implements Initializable {
     private static reddit r = new reddit();
     private static Slack s = new Slack();
+    ObservableList<Bot> bots = FXCollections.observableArrayList();
+    //ArrayList<Bot> bots = new ArrayList<Bot>();
 
     @FXML
     private Label slackVerificationLabel;
     @FXML
     private FontAwesomeIconView slackVerificationIcon;
+    @FXML
+    private TextField newBotName;
+    @FXML
+    private TableView<Bot> botTable;
+    @FXML
+    private TableColumn<Bot, String> nameColumn;
+    @FXML
+    private TableColumn<Bot, CheckBox> statusColumn;
 
     public void initialize(URL location, ResourceBundle resources){
-        System.out.println("Loading user data...");
+        nameColumn.setCellValueFactory(new PropertyValueFactory<Bot, String>("name"));
+        statusColumn.setCellValueFactory(new PropertyValueFactory<Bot, CheckBox>("on"));
+        botTable.setItems(bots);
     }
+    /*public ObservableList<Bot> getBots(){
+        ObservableList<Bot> bots = FXCollections.observableArrayList();
+        b
+        return bots;
+    }*/
     public void slackLoginClicked(){
         s.openWeb2();
         slackVerificationLabel.setText("Verified");
@@ -29,6 +50,14 @@ public class Controller implements Initializable {
         slackVerificationIcon.setFill(Paint.valueOf("#0c8c0c"));
         //slackVerificationIcon.setGlyphStyle("CHECK_CIRCLE_ALT");
         slackVerificationIcon.setGlyphName("CHECK_CIRCLE_ALT");
+    }
+
+    public void onBotCreate(){
+        String name = newBotName.getText();
+        bots.add(new Bot(name));
+        //Bot bot = new Bot(name);
+        //bots.add(bot);
+        //addBotToTable(bot);
     }
 
     public void testRedditRequest(){
